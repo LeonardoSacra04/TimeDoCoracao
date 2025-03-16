2f305f0ec01a7d9ef207324e4c53ab6763896ac1
LeonardoSacra04 <oliveiraleo04@gmail.com>
2025-03-16 13:17:28 -0300

    Escreve o nome do time com as cores: branco, preto e vermelho

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.Write("Meu time do coração é o ");
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("C");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.Write("o");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("r");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.Write("i");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("n");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.Write("t");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("h");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.Write("i");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("a");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.Write("n");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("s");
+Console.ResetColor();
